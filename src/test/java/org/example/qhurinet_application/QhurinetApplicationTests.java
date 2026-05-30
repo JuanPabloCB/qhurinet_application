fbd8b98512d8c2ca6d128083884d6240147077ab
JuanPabloCB <contacto1juanp@gmail.com>
2026-05-30 18:03:08 -0500

    Initial commit

---
+++ src/test/java/org/example/qhurinet_application/QhurinetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qhurinet_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QhurinetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
